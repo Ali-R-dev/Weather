7caea2c75dbef8d1751284b78cc386924f5ddd56
Ali Raza <75201671+Ali-R-dev@users.noreply.github.com>
2025-04-21 15:26:25 +0200

    chore: add Jest + RTL with ts-jest and initial config

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.ts',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
+
+export default config;
---
+++ jest.setup.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+
+// Enable fetch mocking if needed
+// import 'whatwg-fetch';
